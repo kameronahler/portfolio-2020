401132dab0a9873696146214c33db6f892dc1365
Kameron <kamahler@gmail.com>
2020-12-13 16:43:12 -0700

    change to NavLink for activeClass

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import { routes } from '../../routes'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export default function Sidebar() {
   const jsxListItems = routes.map(({ name, path }) => {
     const title = name.charAt(0).toUpperCase() + name.slice(1)
 
     return (
-      <li
-        key={name}
-        className={`app-nav__list-item app-nav__list-item--${name}`}
-      >
-        <Link className={`app-nav__link app-nav__link--${name}`} to={path}>
+      <li key={name} className='app-nav__list-item'>
+        <NavLink
+          className={`app-nav__link app-nav__link--${name}`}
+          activeClassName='app-nav__link--active'
+          to={path}
+        >
           {title}
-        </Link>
+        </NavLink>
       </li>
     )
   })
