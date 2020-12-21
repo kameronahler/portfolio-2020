56a5fcbd27efea59341783f0a05cc7c305f8c963
Kameron <kamahler@gmail.com>
2020-12-20 19:40:20 -0700

    test form

--- src/components/Pages/PageContact.js
+++ src/components/Pages/PageContact.js
@@ -1,18 +1,39 @@
 // packages
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import kwesforms from 'kwesforms'
 
 export default function PageContact() {
+  useEffect(() => {
+    kwesforms.init()
+  }, [])
+
   return (
-    <>
+    <div>
       <h1>Contact</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt
-        aliquam vel laudantium deleniti nobis reprehenderit eaque necessitatibus
-        itaque quasi, nisi voluptas quod ea, dolores quibusdam amet? Qui rem
-        fuga eligendi.
-      </p>
+      <form
+        className='kwes-form'
+        action='https://kwes.io/api/foreign/forms/3VyNJ6R5XfH0mSPKjrFW'
+      >
+        <div>
+          <label htmlFor='name'>Name</label>
+          <input
+            type='text'
+            name='name'
+            data-kw-rules='required|string|min:2'
+          />
+        </div>
+        <div>
+          <label htmlFor='email'>Email</label>
+          <input type='text' name='email' data-kw-rules='required|email' />
+        </div>
+        <div>
+          <label htmlFor='message'>Message</label>
+          <textarea name='message' data-kw-rules='required|string|min:2' />
+        </div>
+        <button type='submit'>Submit</button>
+      </form>
       <Link to='/about'>Previous</Link>
-    </>
+    </div>
   )
 }
