b3368ef3c2d1b682ff09582198d7f9c219d71804
Kameron <kamahler@gmail.com>
2021-04-07 01:06:03 -0600

    feat(PageBlog): abstract contentful types, accessibility, figure, alt

---
+++ src/components/Contentful/RichText.tsx
@@ -0,0 +1,59 @@
+// react
+import React from 'react'
+
+// packages
+import { BLOCKS, Block } from '@contentful/rich-text-types'
+import styled from 'styled-components'
+
+// styled
+const StyledImg = styled.img`
+  display: block;
+  max-width: 100%;
+`
+
+export const RichTextImg = ({ node }: { node: Block }) => {
+  const url = node.data.target.fields.file.url
+  const alt = node.data.target.fields.title
+  const description = node.data.target.fields.description
+  const type = /(?:\.([^.]+))?$/.exec(url)[1]
+  const format = 'fm=webp'
+
+  if (type === '.jpeg' || '.jpg' || '.png') {
+    return (
+      <figure>
+        <StyledImg
+          alt={alt || ''}
+          srcSet={
+            `${url}?${format}&w=200&q=90 200w,` +
+            `${url}?${format}&w=300&q=90 300w,` +
+            `${url}?${format}&w=400&q=80 400w,` +
+            `${url}?${format}&w=800&q=80 800w,` +
+            `${url}?${format}&w=1200&q=70 1200w,` +
+            `${url}?${format}&w=1600&q=70 1600w,` +
+            `${url}?${format}&w=2000&q=60 2000w,` +
+            `${url}?${format}&w=2400&q=60 2400w,` +
+            `${url}?${format}&w=3000&q=60 3000w,` +
+            `${url}?${format}&w=4000&q=60 4000w`
+          }
+          src={url}
+        />
+        {description && <figcaption>{description}</figcaption>}
+      </figure>
+    )
+  } else {
+    return (
+      <figure>
+        <StyledImg alt={alt || ''} src={url} />
+        {description && <figcaption>{description}</figcaption>}
+      </figure>
+    )
+  }
+}
+
+export const CONTENTFUL_RICH_TEXT_OPTIONS = {
+  renderNode: {
+    [BLOCKS.EMBEDDED_ASSET]: (node: Block) => {
+      return <RichTextImg node={node} />
+    },
+  },
+}
--- src/components/Pages/PageBlog.tsx
+++ src/components/Pages/PageBlog.tsx
@@ -4,48 +4,19 @@ import { Link } from 'react-router-dom'
 
 // packages
 import { EntryCollection } from 'contentful'
-import { BLOCKS } from '@contentful/rich-text-types'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
 // hooks
 import { useFetchContentful } from '../../hooks/hooks'
 
 // components
+import { CONTENTFUL_RICH_TEXT_OPTIONS } from '../Contentful/RichText'
 import { Loader } from '../Loader/Loader'
 
 // constants
 const CONTENTFUL_ENTRY_TYPE = 'blogPost'
-const CONTENTFUL_RICH_TEXT_OPTIONS = {
-  renderNode: {
-    [BLOCKS.EMBEDDED_ASSET]: node => {
-      const url = node.data.target.fields.file.url
-      const type = /(?:\.([^.]+))?$/.exec(url)[1]
 
-      if (type === '.jpeg' || '.jpg' || '.png') {
-        return (
-          <img
-            style={{ maxWidth: '100%', display: 'block' }}
-            srcSet={
-              `${url}?fm=webp&w=200&q=90 200w,` +
-              `${url}?fm=webp&w=300&q=90 300w,` +
-              `${url}?fm=webp&w=400&q=80 400w,` +
-              `${url}?fm=webp&w=800&q=80 800w,` +
-              `${url}?fm=webp&w=1200&q=70 1200w,` +
-              `${url}?fm=webp&w=1600&q=70 1600w,` +
-              `${url}?fm=webp&w=2000&q=60 2000w,` +
-              `${url}?fm=webp&w=2400&q=60 2400w,` +
-              `${url}?fm=webp&w=3000&q=60 3000w,` +
-              `${url}?fm=webp&w=4000&q=60 4000w`
-            }
-            src={url}
-          />
-        )
-      } else {
-        return <img src={url} />
-      }
-    },
-  },
-}
+// styled
 export const PageBlog = React.memo(() => {
   const mounted = useRef<Boolean>(true)
   const [
@@ -61,7 +32,6 @@ export const PageBlog = React.memo(() => {
     })
   }, [])
 
-  console.log(contentfulEntries)
   return (
     <>
       <h1>blogPost</h1>
