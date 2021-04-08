e2794a148652424eb5292549b4c4b5828a54b6e0
Kameron <kamahler@gmail.com>
2021-04-08 00:01:43 -0600

    chore(Page): consolidate styled components back into Page

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -3,15 +3,51 @@ import React from 'react'
 import { Route, Switch, useLocation } from 'react-router-dom'
 import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
+// packages
+import styled from 'styled-components'
+
 // routes
 import { routes } from '../../routes'
 
 // theme
 import { THEME } from '../../styles/GlobalTheme'
 
-// components
-import { Page__Wrapper } from './Page__Wrapper'
-import { Page__Transition } from './Page__Transition'
+// styled
+const StyledPage__Wrapper = styled.div`
+  background-color: var(--color-bg);
+  height: 100vh;
+  overflow-y: auto;
+  padding: 2rem;
+  position: relative;
+`
+
+const StyledPage__Transition = styled.div`
+  &.enter-active,
+  &.exit-active {
+    transition-duration: var(--duration-page-transition-ms);
+    transition-property: opacity, transform;
+  }
+
+  &.enter {
+    opacity: 0.01;
+    transform: translateX(2rem);
+  }
+
+  &.enter-active {
+    opacity: 1;
+    transform: translateX(0);
+    transition-delay: var(--duration-page-transition-ms);
+  }
+
+  &.exit-active {
+    left: 0;
+    opacity: 0;
+    position: absolute;
+    top: 0;
+    transform: translateX(2rem);
+    transition-timing-function: ease-in;
+  }
+`
 
 // constants
 const PAGE_TRANSITION_DURATION = +THEME.duration.pageTransition * 2
@@ -20,10 +56,10 @@ export const Page = () => {
   const location = useLocation()
 
   return (
-    <Page__Wrapper>
+    <StyledPage__Wrapper>
       <TransitionGroup style={{ position: 'relative' }}>
         <CSSTransition key={location.key} timeout={PAGE_TRANSITION_DURATION}>
-          <Page__Transition>
+          <StyledPage__Transition>
             <Switch location={location}>
               {routes.map(({ name, component, path }) => {
                 return (
@@ -31,9 +67,9 @@ export const Page = () => {
                 )
               })}
             </Switch>
-          </Page__Transition>
+          </StyledPage__Transition>
         </CSSTransition>
       </TransitionGroup>
-    </Page__Wrapper>
+    </StyledPage__Wrapper>
   )
 }
