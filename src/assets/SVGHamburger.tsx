2df28d4ade524eb9680c8cc417a2beb6cf345243
Kameron <kamahler@gmail.com>
2021-04-13 11:43:56 -0600

    chore(svgs): convert to imported svgs to help with noise

---
+++ src/assets/SVGHamburger.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const SVGHamburger = (
+  <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 32 32'>
+    <path fill='var(--color-bg)' d='M0 0h32v32H0z' />
+    <path fill='var(--color-text)' d='M0 32L32 0H0v32z' />
+  </svg>
+)
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -4,6 +4,7 @@ import { NavLink, useLocation } from 'react-router-dom'
 
 // packages
 import styled from 'styled-components'
+import { SVGHamburger } from '../../assets/SVGHamburger'
 
 // theme
 import { THEME } from '../../styles/Theme'
@@ -88,10 +89,7 @@ export const Nav = () => {
         mobileNavOpen={mobileNavOpen}
         onClick={() => setMobileNavOpen(!mobileNavOpen)}
       >
-        <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 32 32'>
-          <path fill='var(--color-bg)' d='M0 0h32v32H0z' />
-          <path fill='var(--color-text)' d='M0 32L32 0H0v32z' />
-        </svg>
+        {SVGHamburger}
       </StyledMobileButton>
 
       <StyledNav
