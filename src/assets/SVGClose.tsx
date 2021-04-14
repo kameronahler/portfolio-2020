fbbbebca2ebc2efa00b7cbf6e895b0a51e3d8355
Kameron <kamahler@gmail.com>
2021-04-14 12:38:28 -0600

    feat(svgs): add two new svgs

---
+++ src/assets/SVGClose.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const SVGClose = (
+  <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 48 48'>
+    <rect
+      width='24'
+      height='3'
+      x='16.58'
+      y='14.95'
+      fill='#252427'
+      rx='1.5'
+      transform='rotate(45 16.58 14.95)'
+    />
+    <rect
+      width='24'
+      height='3'
+      x='14.45'
+      y='31.92'
+      fill='#252427'
+      rx='1.5'
+      transform='rotate(-45 14.45 31.92)'
+    />
+  </svg>
+)
--- src/assets/SVGHamburger.tsx
+++ src/assets/SVGHamburger.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
 export const SVGHamburger = (
-  <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 32 32'>
-    <path fill='transparent' d='M0 0h32v32H0z' />
-    <path fill='var(--color-text)' d='M0 32L32 0H0v32z' />
+  <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 48 48'>
+    <rect width='20' height='3' x='14' y='15' fill='#252427' rx='1.5' />
+    <rect width='24' height='3' x='12' y='23' fill='#252427' rx='1.5' />
+    <rect width='20' height='3' x='14' y='31' fill='#252427' rx='1.5' />
   </svg>
 )
