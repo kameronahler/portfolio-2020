35036c7d3d7e93652683054f3e95fc4e0d15c142
Kameron <kamahler@gmail.com>
2021-04-14 16:34:24 -0600

    chore(svgs): fix attrs

--- src/assets/SVGArrowTopRight.tsx
+++ src/assets/SVGArrowTopRight.tsx
@@ -4,9 +4,9 @@ export const SVGArrowTopRight = (
   <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 32 32'>
     <path
       fill='#252427'
-      fill-rule='evenodd'
+      fillRule='evenodd'
       d='M26.73 6.78a1 1 0 00-1.25-1.25L14.47 8.94a1 1 0 00-.41 1.66l3.13 3.14L4.47 26.46a.75.75 0 001.06 1.06L18.25 14.8l3.4 3.4a1 1 0 001.67-.4l3.41-11.02z'
-      clip-rule='evenodd'
+      clipRule='evenodd'
     />
   </svg>
 )
--- src/assets/SVGResume.tsx
+++ src/assets/SVGResume.tsx
@@ -4,9 +4,9 @@ export const SVGResume = (
   <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 32 32'>
     <path
       stroke='#ECEBF5'
-      stroke-linecap='round'
-      stroke-linejoin='round'
-      stroke-width='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth='2'
       d='M25 14h-6M25 20H7M21 26H7'
     />
     <path
@@ -15,24 +15,24 @@ export const SVGResume = (
     />
     <path
       stroke='#ECEBF5'
-      stroke-linecap='round'
-      stroke-linejoin='round'
-      stroke-width='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth='1.5'
       d='M6.8 4H2.93v0C1.87 4 1 4.86 1 5.93v23.14C1 30.14 1.87 31 2.93 31h25.14v0c1.06 0 1.93-.86 1.93-1.93V5.93C30 4.86 29.13 4 28.07 4H14.53'
     />
     <path
       stroke='#252427'
-      stroke-linecap='round'
-      stroke-linejoin='round'
-      stroke-width='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth='1.5'
       d='M7 3H3v0a2 2 0 00-2 2v24c0 1.1.9 2 2 2h26v0a2 2 0 002-2V5a2 2 0 00-2-2H15'
     />
     <path fill='#4C36D3' d='M15 15l-4-3-4 3V2a1 1 0 011-1h6a1 1 0 011 1v13z' />
     <path
       stroke='#252427'
-      stroke-linecap='round'
-      stroke-linejoin='round'
-      stroke-width='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth='1.5'
       d='M7 25h14M7 19h18M25 13h-6'
     />
   </svg>
