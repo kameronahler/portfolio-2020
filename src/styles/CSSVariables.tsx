c25585dc1dadb3284a23e77a78c64d0650ddaa20
Kameron <kamahler@gmail.com>
2021-05-13 14:51:35 -0600

    fix(CSSVariables): errant bracket

--- src/styles/CSSVariables.tsx
+++ src/styles/CSSVariables.tsx
@@ -25,6 +25,7 @@ export const CSSVariables = createGlobalStyle`
   :root {
     ${getVars().join('')}
   }
+
   html.darkmode {
     --color-accent: #3DD8F0;
     --color-bg-accent: #161329;
@@ -36,11 +37,10 @@ export const CSSVariables = createGlobalStyle`
     --color-primary-light: #a293ff;
     --color-primary: #735DF7;
     --color-text-inverse: rgba(0, 0, 0, .95);
-    --color-text-light: #E5E1F5;
+    --color-text-light: #BAB6C6;
     --color-text: #d6d3e5;
     --color-white: #ffffff;
     --color-warning: #F75C78;
     --shadow-card: 0 8px 24px -8px rgba(0,0,0,1);
-    }
   }
 `
