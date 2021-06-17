9882a1e98a5f878aea26c9e8a2479cac28a8a530
Kameron <kamahler@gmail.com>
2021-06-16 23:00:27 -0600

    refactor(formium): delete existing formium code

--- src/lib/formium.tsx
+++
@@ -1,8 +0,0 @@
-import { createClient, FormiumClient } from '@formium/client'
-
-export const formium: FormiumClient = createClient(
-  process.env.FORMIUM_PROJECT_ID,
-  {
-    apiToken: process.env.FORMIUM_API_TOKEN,
-  }
-)
